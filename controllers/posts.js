54379a9ce9b1b1a68d1843fbe8a3dd24646242d0
Felix Anducho <felix.anducho74@gmail.com>
2020-07-01 19:24:15 -0500

    Add the functionality of upload a image with multer

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,5 +1,34 @@
 const postsRouter = require('express').Router()
 const Post = require('../models/post')
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads/')
+  },
+  filename: (req, file, cb) => {
+    cb(null,new Date().toISOString() + file.originalname)
+  }
+
+})
+
+const fileFilter = (req, file, cb) => {
+  if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+    //accept file
+    cb(null,true)
+  } else {
+  //reject a file
+  cb(null,false)
+  }
+}
+
+const upload = multer({
+  storage: storage, 
+  limits: {
+    fileSize: 1024 * 1024 * 3
+  },
+  fileFilter: fileFilter
+})
 
 postsRouter.get('/api/posts', async (req, res) => {
     const posts = await Post.find({})
@@ -7,13 +36,15 @@ postsRouter.get('/api/posts', async (req, res) => {
     res.json(posts.map(post => post.toJSON()))
 })
 
-postsRouter.post('/api/posts', async (req, res) => {
+postsRouter.post('/api/posts', upload.single('postImage') ,async (req, res) => {
+  console.log(req.file)
     try {
         const body = req.body
 
         const post = new Post({
             title: body.title,
             author: body.author,
+            postImage: req.file.path,
             replies: []
         })
         
